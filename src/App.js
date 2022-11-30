69a882bb6d363d69a6411fdd1f33b1709d697337
hyelim <vkfks4472889@gmail.com>
2022-11-30 23:49:19 +0900

    [#1] init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");// api 호출시 state가 변경되었을 때 render되길 원하지 않고, 첫번째 render만 원한다
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []); // 코드가 딱 한번만 render되도록 함
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Welcome back!!!!!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button text={"버튼"}/>
     </div>
   );
 }
